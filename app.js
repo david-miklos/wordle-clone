9b134bc25b11ef21ffe9954debfad4df127088ee
david-miklos <david.miklos@siemens.com>
2022-10-07 11:59:54 +0300

    added game logic

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const squares = document.querySelectorAll(".board-square");
+const squares = document.querySelectorAll('.board-square');
 const ANSWER_LENGTH = 5;
-let currentGuess = "";
+const ROUNDS = 6;
+let currentGuess = '';
 let round = 0;
+let done = false;
 
-function init() {
-  document.addEventListener("keydown", function handleKeyPress(event) {
+async function init() {
+  word = await getWordOfTheDay();
+  console.log(word)
+
+  document.addEventListener('keydown', function handleKeyPress(event) {
+    if (done) {
+      return;
+    }
+
     const action = event.key;
 
-    if (action === "Enter") {
+    if (action === 'Enter') {
       commitAnswer();
     }
 
-    if (action === "Backspace") {
+    if (action === 'Backspace') {
       backspace();
     }
 
@@ -36,16 +45,110 @@ function displayLetter(letter) {
 function backspace() {
   currentGuess = currentGuess.slice(0, -1);
   // remove letter from correct sqaure
-  squares[round * ANSWER_LENGTH + currentGuess.length].innerText = "";
+  squares[round * ANSWER_LENGTH + currentGuess.length].innerText = '';
 }
 
-function commitAnswer() {
+async function commitAnswer() {
   if (currentGuess.length === ANSWER_LENGTH) {
-    // TODO validate word
+    if (!(await isValid(currentGuess))) {
+      flash();
+      return;
+    }
 
-    currentGuess = "";
+    wordMap = makeMap(word);
+
+    markWord();
+
+    if (currentGuess === word) {
+      done = true;
+      alert("You win! Congrats!")
+      return
+    }
+    
     round++;
+    currentGuess = '';
+    wordMap = makeMap(word);
+    
+    if(round === ROUNDS){
+      done = true;
+      alert(`You lost! The word of the day was ${word.toUpperCase()}.`)
+    }
   }
+}
+
+async function isValid(quess) {
+  // The API will return back to you the word you sent and validWord which will be true or false. e.g.
+  // { "word": "crane", "validWord": true } or { "word": "asdfg", "validWord": false }.
+  const res = await fetch('https://words.dev-apis.com/validate-word', {
+    method: 'POST',
+    body: JSON.stringify({ word: quess }),
+  });
+  const { validWord } = await res.json();
+  return validWord;
+}
+
+async function getWordOfTheDay() {
+  // The response will look like this: {"word":"humph","puzzleNumber":3}
+  // where the word is the current word of the day and the puzzleNumber is which puzzle of the day it is
+  const res = await fetch('https://words.dev-apis.com/word-of-the-day');
+  const { word } = await res.json();
+  return word;
+}
+
+function flash() {
+  // We are removing it and adding it back again to flash more than 1 time
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    squares[round * ANSWER_LENGTH + i].classList.remove('error');
+  }
+  setTimeout(function addClass() {
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      squares[round * ANSWER_LENGTH + i].classList.add('error');
+    }
+  }, 10);
+}
+
+function markWord() {
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    if (currentGuess.charAt(i) === word.charAt(i)) {
+      wordMap[currentGuess.charAt(i)]--;
+      markCorrect(i);
+    } else if (
+      word.includes(currentGuess.charAt(i)) &&
+      wordMap[currentGuess.charAt(i)] > 0
+    ) {
+      wordMap[currentGuess.charAt(i)]--;
+      markClose(i);
+    } else {
+      markWrong(i);
+    }
+  }
+}
+
+function markCorrect(position) {
+  squares[round * ANSWER_LENGTH + position].classList.add('correct');
+}
+
+function markClose(position) {
+  squares[round * ANSWER_LENGTH + position].classList.add('close');
+}
+
+function markWrong(position) {
+  squares[round * ANSWER_LENGTH + position].classList.add('wrong');
+}
+
+// takes a word (like 'boost') and creates an object out of it
+// (like {b: 1, o: 2, s: 1, t: 1}) so we can use that to
+// make sure we get the correct amount of letters marked close
+function makeMap(word) {
+  obj = {};
+  for (let i = 0; i < word.length; i++) {
+    if (word.charAt(i) in obj) {
+      obj[word.charAt(i)]++;
+      continue;
+    }
+    obj[word.charAt(i)] = 1;
+  }
+  return obj;
 }
 
 init();
